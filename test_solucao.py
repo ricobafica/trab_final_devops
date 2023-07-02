dde2ab4992d5f49f87b1cc2f9e392ce59e31e0cf
Ricardo Bafica Pontes <ricardo.bafica@gmail.com>
2023-07-02 11:13:54 -0300

    incluir assert no script de teste

--- test_solucao.py
+++ test_solucao.py
@@ -2,7 +2,10 @@
 import src.bhaskara.eq_bhaskara as eq_bhaskara
 
 print("estou no arquivo que testa bhaskara")
-eq_bhaskara.raizes(1, 5, -6)
+r1, r2 = eq_bhaskara.raizes(1, 5, -6)
+
+assert (1*r1**2 + 5*r1 + -6) == 0
+assert (1*r2**2 + 5*r2 + -6) == 0
 
 # Good Integration Practices
 # https://docs.pytest.org/en/latest/explanation/goodpractices.html#goodpractices
