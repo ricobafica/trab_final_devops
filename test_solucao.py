506104b33cbe9f46cc4b0d0129e6942143c45a55
Ricardo Bafica Pontes <ricardo.bafica@gmail.com>
2023-07-02 12:15:08 -0300

    funcoes dedicadas a cada rais da eq 2 grau

--- test_solucao.py
+++ test_solucao.py
@@ -1,21 +1,20 @@
-# import src.modraizesquadraticas.raizesquadraticas as raizesquadraticas
 import src.bhaskara.eq_bhaskara as eq_bhaskara
 
 
-def calc(raiz):
+def test_rais1():
     print("estou no arquivo que testa bhaskara")
     r1, r2 = eq_bhaskara.raizes(1, 5, -6)
-    if raiz == 1:
-        return (1*r1**2 + 5*r1 + -6)
-    else:
-        return (1*r2**2 + 5*r2 + -6)
+    assert (1*r1**2 + 5*r1 + -6) == 0
 
 
-def test_bhaskara():
-    assert calc(1) == 0
-    assert calc(2) == 0
+def test_raiz2():
+    print("estou no arquivo que testa bhaskara")
+    r1, r2 = eq_bhaskara.raizes(1, 5, -6)
+    # return (1*r2**2 + 5*r2 + -6)
+    assert (1*r2**2 + 5*r2 + -6) == 0
 
-#    assert (1*r2**2 + 5*r2 + -6) == 0
+# Why PyTest is not collecting tests (collected 0 items)?
+# https://stackoverflow.com/questions/37353960/why-pytest-is-not-collecting-tests-collected-0-items
 
 # Good Integration Practices
 # https://docs.pytest.org/en/latest/explanation/goodpractices.html#goodpractices
