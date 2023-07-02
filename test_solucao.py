2971f7d0a162ff9c95a184868862c7b87808cd91
Ricardo Bafica Pontes <ricardo.bafica@gmail.com>
2023-07-02 11:44:07 -0300

    inclusao de funcao no codigo

--- test_solucao.py
+++ test_solucao.py
@@ -1,11 +1,21 @@
 # import src.modraizesquadraticas.raizesquadraticas as raizesquadraticas
 import src.bhaskara.eq_bhaskara as eq_bhaskara
 
-print("estou no arquivo que testa bhaskara")
-r1, r2 = eq_bhaskara.raizes(1, 5, -6)
 
-assert (1*r1**2 + 5*r1 + -6) == 0
-assert (1*r2**2 + 5*r2 + -6) == 0
+def calc(raiz):
+    print("estou no arquivo que testa bhaskara")
+    r1, r2 = eq_bhaskara.raizes(1, 5, -6)
+    if raiz == 1:
+        return (1*r1**2 + 5*r1 + -6)
+    else:
+        return (1*r2**2 + 5*r2 + -6)
+
+
+def test_bhaskara():
+    assert calc(1) == 0
+    assert calc(2) == 0
+
+#    assert (1*r2**2 + 5*r2 + -6) == 0
 
 # Good Integration Practices
 # https://docs.pytest.org/en/latest/explanation/goodpractices.html#goodpractices
