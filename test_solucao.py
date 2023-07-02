ad9f432b25bca5b2ca931b91827cc286581f9454
Ricardo Bafica Pontes <ricardo.bafica@gmail.com>
2023-07-01 21:43:08 -0300

    novas tentativas de pytest

--- test_solucao.py
+++ test_solucao.py
@@ -3,3 +3,6 @@ import src.bhaskara.eq_bhaskara as eq_bhaskara
 
 print("estou no arquivo que testa bhaskara")
 eq_bhaskara.raizes(1, 5, -6)
+
+# Good Integration Practices
+# https://docs.pytest.org/en/latest/explanation/goodpractices.html#goodpractices
